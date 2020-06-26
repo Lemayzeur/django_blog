9e2ae9e50f0b23011ea3c33485afba72bd1c53e2
lemayzeur <lemayzeur@code9haiti.com>
2020-06-26 09:27:57 -0400

    First model creation

--- index/models.py
+++ index/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Atik(models.Model):
+	jounalis = models.CharField(max_length=100)
+	tit = models.CharField(max_length=255)
+	kontni = models.TextField(max_length=4000)
+	dat_pibliye = models.DateField(default=timezone.now)
